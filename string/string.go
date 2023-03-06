aab69d7df2e10d4082414bb564fea56668520b72
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-03-06 13:39:20 +0700

    .

---
+++ string/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Rafli")
+	fmt.Println("Rafli Bima")
+	fmt.Println("Rafli Bima Pratandra")
+
+	fmt.Println(len("Rafli")) // func len untuk mencari jumlah string (Termasuk spasi terhitung)
+	fmt.Println("Rafli Bima"[0])
+	fmt.Println("Rafli Bima Pratandra"[1])
+}
