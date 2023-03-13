6d0aea1a762029a440796f8c4e42e57dbd4bc849
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-03-13 15:08:35 +0700

    add variable

---
+++ variable/variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Rafli Bima"
+	fmt.Println(name)
+
+	name = "Rafli Pratandra"
+	fmt.Println(name)
+
+	var friendName = "Budi"
+	fmt.Println(friendName)
+
+	var age = 20
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Rafli"
+		lastName  = "Bima"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
