59400d0a86410686660ebc2639c7c5a90ec78ac8
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-03-15 12:01:46 +0700

    .

---
+++ array/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [3]string
+
+	names[0] = "Rafli"
+	names[1] = "Bima"
+	names[2] = "Pratandra"
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+
+	var values = [3]int{90, 95, 100}
+
+	fmt.Println(values[0])
+	fmt.Println(values[1])
+	fmt.Println(values[2])
+}
